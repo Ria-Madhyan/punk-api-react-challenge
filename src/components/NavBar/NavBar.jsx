e1fd7bdf997f66da96b7cce37a105d6b3f5c1733
Ria Madhyan <ria.madhyan@db.com>
2021-11-11 10:50:53 +0530

    initial commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <label htmlFor="high-abv">High ABV(6.0%)</label>
+      <input type="checkbox" id="high-abv"></input>
+      
+    </div>
+  )
+}
+
+export default NavBar
